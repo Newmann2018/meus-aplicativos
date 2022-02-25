ed732275a997f775f902ed89f2a3c8deae341dc7
newmann Naba Costa <99230382+Newmann2018@users.noreply.github.com>
2022-02-25 10:22:15 -0300

    meu primeiro app
    fazendo telas

--- APP1/android/app/src/main/java/com/app1/MainActivity.java
+++ APP1/android/app/src/main/java/com/app1/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "APP1";
+    return "App1";
   }
 }
--- APP1/ios/APP1Tests/APP1Tests.m
+++ APP1/ios/APP1Tests/APP1Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface APP1Tests : XCTestCase
+@interface App1Tests : XCTestCase
 
 @end
 
-@implementation APP1Tests
+@implementation App1Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
